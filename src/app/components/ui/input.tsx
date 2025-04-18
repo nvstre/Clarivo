6c5972e53c69915846a0783d9d68979023541e26
nvstre <mbiraut@gmail.com>
2025-04-18 16:56:42 +0200

    Fixed vercel errors

--- src/app/components/ui/input.tsx
+++ src/app/components/ui/input.tsx
@@ -1,9 +1,10 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  // You can define any custom props here if needed
+  className?: string;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -20,6 +21,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     )
   }
 )
+
 Input.displayName = "Input"
 
 export { Input }
